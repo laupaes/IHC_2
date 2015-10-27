146bd96884e87739bce6c131a3234d6deaa76562
laupaes <lau.paes@gmail.com>
2015-10-27 09:43:47 -0200

    primeiro commit

---
+++ platforms/android/assets/www/app/app.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('deviceready', deviceReady, false);
+
+$(document).ready(function () {
+        deviceReady();
+});
+
+
+function deviceReady() {
+    $("#splash").load("app/main.html");
+}
